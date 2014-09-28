9591fd7f083ca61712ac229ac1fa53886b7b2b67
Duncan Miller <duncanjmiller@gmail.com>
2014-09-28 13:33:02 -0700

    creating rails 4 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Openshiro::Application.config.secret_key_base = 'b5e1e1d20feb74cf3d2a6c86ad812345fe4735d82388274c8fdbd97024e660be1b4508ffcdf62f635f19255e2e5f0da22c358b3436155e6b86a182d98ecc2ff7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Openshiro::Application.config.session_store :cookie_store, key: '_openshiro_session'
