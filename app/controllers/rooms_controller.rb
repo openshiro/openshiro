dcedc7d9eb319d459058d3ab639f0a8348603de4
Duncan Miller <duncanjmiller@gmail.com>
2014-09-29 16:05:58 -0700

    copying rooms controller

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,74 +1,110 @@
 class RoomsController < ApplicationController
-  before_action :set_room, only: [:show, :edit, :update, :destroy]
+  before_filter :config_opentok,:except => [:index]
 
-  # GET /rooms
-  # GET /rooms.json
   def index
     @rooms = Room.all
+    @new_room = Room.new
   end
 
-  # GET /rooms/1
-  # GET /rooms/1.json
-  def show
-  end
-
-  # GET /rooms/new
-  def new
-    @room = Room.new
-  end
-
-  # GET /rooms/1/edit
-  def edit
-  end
-
-  # POST /rooms
-  # POST /rooms.json
   def create
-    @room = Room.new(room_params)
-
+    session = @opentok.create_session request.remote_addr
+    params[:room][:sessionId] = session.session_id
+    @new_room = Room.new(params[:room])
     respond_to do |format|
-      if @room.save
-        format.html { redirect_to @room, notice: 'Room was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @room }
+      if @new_room.save
+        format.html { redirect_to("/party/"+@new_room.id.to_s) }
       else
-        format.html { render action: 'new' }
-        format.json { render json: @room.errors, status: :unprocessable_entity }
+        format.html { render :controller => 'rooms',
+  :action => "index" }
       end
     end
   end
 
-  # PATCH/PUT /rooms/1
-  # PATCH/PUT /rooms/1.json
-  def update
-    respond_to do |format|
-      if @room.update(room_params)
-        format.html { redirect_to @room, notice: 'Room was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @room.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /rooms/1
-  # DELETE /rooms/1.json
-  def destroy
-    @room.destroy
-    respond_to do |format|
-      format.html { redirect_to rooms_url }
-      format.json { head :no_content }
-    end
+  def party
+    @room = Room.find(params[:id])
+    @tok_token = @opentok.generate_token :session_id =>@room.sessionId
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_room
-      @room = Room.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def room_params
-      params.require(:room).permit(:name, :sessionID)
+  def config_opentok
+    if @opentok.nil?
+      @opentok = OpenTok::OpenTokSDK.new 22329432, "f03a315fc996dff095d697eb7949cbec1474c6ba"
     end
+  end
 end
+
+# class RoomsController < ApplicationController
+#   before_action :set_room, only: [:show, :edit, :update, :destroy]
+#
+#   # GET /rooms
+#   # GET /rooms.json
+#   def index
+#     @rooms = Room.all
+#   end
+#
+#   # GET /rooms/1
+#   # GET /rooms/1.json
+#   def show
+#   end
+#
+#   # GET /rooms/new
+#   def new
+#     @room = Room.new
+#   end
+#
+#   # GET /rooms/1/edit
+#   def edit
+#   end
+#
+#   # POST /rooms
+#   # POST /rooms.json
+#   def create
+#     @room = Room.new(room_params)
+#
+#     respond_to do |format|
+#       if @room.save
+#         format.html { redirect_to @room, notice: 'Room was successfully created.' }
+#         format.json { render action: 'show', status: :created, location: @room }
+#       else
+#         format.html { render action: 'new' }
+#         format.json { render json: @room.errors, status: :unprocessable_entity }
+#       end
+#     end
+#   end
+#
+#   # PATCH/PUT /rooms/1
+#   # PATCH/PUT /rooms/1.json
+#   def update
+#     respond_to do |format|
+#       if @room.update(room_params)
+#         format.html { redirect_to @room, notice: 'Room was successfully updated.' }
+#         format.json { head :no_content }
+#       else
+#         format.html { render action: 'edit' }
+#         format.json { render json: @room.errors, status: :unprocessable_entity }
+#       end
+#     end
+#   end
+#
+#   # DELETE /rooms/1
+#   # DELETE /rooms/1.json
+#   def destroy
+#     @room.destroy
+#     respond_to do |format|
+#       format.html { redirect_to rooms_url }
+#       format.json { head :no_content }
+#     end
+#   end
+#
+#   private
+#     # Use callbacks to share common setup or constraints between actions.
+#     def set_room
+#       @room = Room.find(params[:id])
+#     end
+#
+#     # Never trust parameters from the scary internet, only allow the white list through.
+#     def room_params
+#       params.require(:room).permit(:name, :sessionID)
+#     end
+# end
