11fab618e56c0b7c949196635233e38fc5407ac9
Duncan Miller <duncanjmiller@gmail.com>
2014-09-29 20:49:46 -0700

    fixing http basic auth

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Openshiro::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   #http basic site wide authentication
-config.middleware.insert_after(::Rack::Lock, "::Rack::Auth::Basic", "Staging") do |u, p|
-  [u, p] == ['vermont', 'vermont']
+  config.middleware.insert_before(Rack::Runtime, "::Rack::Auth::Basic", "Staging") do |u, p|
+    [u, p] == ['vermont', 'vermont']
   end
 end
